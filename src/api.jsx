4286ab9229d8f0654aa6297c9814b54c43508d23
Yusuf <yshaikh113@outlook.com>
2024-07-30 12:37:22 -0500

    Fixed API URL

--- src/api.jsx
+++ src/api.jsx
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:4000'
-  : 'https://react-express-portfolio-final-backend.onrender.com';
+  : 'https://react-express-portfolio-final.vercel.app';
 
   export default API_BASE_URL;

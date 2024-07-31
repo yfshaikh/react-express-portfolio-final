7ede289720cdafd6e97b3c8e08201031e454d3df
Yusuf <yshaikh113@outlook.com>
2024-07-31 07:24:23 -0500

    added missing import

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -6,6 +6,7 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Editor from '../components/Editor'
 import '../styles/Quill.css'
+import API_BASE_URL from '../api'
 
 
 

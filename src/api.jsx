5371081cfb220bd7dbc06c37621c932895ce07ee
Yusuf <yshaikh113@outlook.com>
2024-08-02 11:38:59 -0500

    revreted some api route changes

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 const API_BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:4000'
-  : 'https://react-express-portfolio-final.vercel.app/api';
+  : 'https://react-express-portfolio-final.vercel.app';
   
 
   export default API_BASE_URL;

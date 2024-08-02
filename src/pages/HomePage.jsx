11fab093dfd83dd7a12b9d73f1130197901f9de1
Yusuf <yshaikh113@outlook.com>
2024-08-02 10:37:54 -0500

    added conditional loading

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,11 +8,12 @@ import { useEffect, useState } from 'react'
 import API_BASE_URL from '../api.jsx'
 
 function HomePage() {
-  
   // dynamic posts 
-  const[posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([])
   const [images, setImages] = useState({}) // To store image URLs
-  
+  const [loadingPosts, setLoadingPosts] = useState(true)
+  const [loadingImages, setLoadingImages] = useState(true)
+
   useEffect(() => {
     // Fetch posts
     fetch(`${API_BASE_URL}/post`, {
@@ -22,19 +23,28 @@ function HomePage() {
       .then(response => response.json())
       .then(posts => {
         setPosts(posts);
+        setLoadingPosts(false);
 
         // Fetch images for each post
-        posts.forEach(post => {
+        const imagePromises = posts.map(post => {
           if (post.file) {
-            fetchImage(post.file);
+            return fetchImage(post.file);
           }
+          return Promise.resolve();
         });
+
+        // Set loadingImages to false once all images are fetched
+        Promise.all(imagePromises).then(() => setLoadingImages(false));
       })
-      .catch(error => console.error('Error fetching posts:', error));
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+        setLoadingPosts(false);
+        setLoadingImages(false);
+      });
 
     // Fetch image data
     function fetchImage(fileId) {
-      fetch(`${API_BASE_URL}/file/${fileId}`, {
+      return fetch(`${API_BASE_URL}/file/${fileId}`, {
         credentials: 'include',
       })
         .then(response => {
@@ -50,33 +60,28 @@ function HomePage() {
         .catch(error => console.error('Error fetching image:', error));
     }
   }, []);
-  
-      
+
+  if (loadingPosts || loadingImages) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-        
-        <Header />
-
-        <div className="row-container"><Intro title="👋 Hey! I'm Yusuf"/></div>
-
-        <div className="row-container"><AboutMe /></div>
-
-        <h2 className='section-heading subtitle'>Projects</h2>
-        <div className='row-container'>
-            {posts.length > 0 && posts.map((post, index) => (
-              //pass all the props of a post to the Card component
-              <Card key={post._id} {...post} image={images[post.file]} />
-            ))}
-        </div>
-        
-        <h2 className='section-heading subtitle'>Experience</h2>
-        <div className='row-container'><Timeline /></div>
-
-        <Footer />
-                            
+      <Header />
+      <div className="row-container"><Intro title="👋 Hey! I'm Yusuf" /></div>
+      <div className="row-container"><AboutMe /></div>
+      <h2 className='section-heading subtitle'>Projects</h2>
+      <div className='row-container'>
+        {posts.length > 0 && posts.map((post, index) => (
+          //pass all the props of a post to the Card component
+          <Card key={post._id} {...post} image={images[post.file]} />
+        ))}
+      </div>
+      <h2 className='section-heading subtitle'>Experience</h2>
+      <div className='row-container'><Timeline /></div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
 import envCompatible from "vite-plugin-env-compatible";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: 'REACT-APP_',
   build: {
@@ -15,7 +14,6 @@ export default defineConfig({
     svgrPlugin({
       svgrOptions: {
         icon: true,
-        // ...svgr options (https://react-svgr.com/docs/options/)
       },
     }),
   ],

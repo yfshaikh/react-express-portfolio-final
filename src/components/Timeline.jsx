74fc88063b8dc7c6a755cf67c668f806c2226f99
Yusuf <yshaikh113@outlook.com>
2024-08-04 10:36:45 -0500

    updated timeline display

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -10,7 +10,7 @@ import '../styles/Timeline.css'
             <div className="timeline-content">
                 <h3>{title}</h3>
                 <h4>{subtitle}</h4>
-                <p><div dangerouslySetInnerHTML={{ __html: description }} /></p>
+                <div dangerouslySetInnerHTML={{ __html: description }} />
             </div>
           </div>
         </div> 

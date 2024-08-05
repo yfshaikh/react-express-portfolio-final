1c9f2eba1fdae19831f55a95c5b3f4a8f70635fc
Yusuf <yshaikh113@outlook.com>
2024-08-05 08:43:25 -0500

    updated timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -3,14 +3,20 @@ import '../styles/Timeline.css';
 
 const Timeline = ({ title, subtitle, description, startDate, endDate, _id }) => {
   return (
-    <div className="timeline-component" key={_id}>
-      <div className="timeline-line"></div>
-      <div className="timeline-content">
-        <h3>{title}</h3>
-        <h4>{subtitle}</h4>
-        <p>{description}</p>
+    <>
+      <div class="timeline-line"></div>
+      <div class="timeline-circle"></div>
+      <div class="timeline-component" key={_id}>
+        <div class="timeline-content">
+          <h3>{title}</h3>
+          <h4>{subtitle}</h4>
+          <p>{description}</p>
+          <h5>{startDate} - {endDate}</h5>
+        </div>
       </div>
-    </div>
+    </>
+        
+
   );
 };
 

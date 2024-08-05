d1c8a19723d53f9f3259bb0609a9f17d30871aa7
Yusuf <yshaikh113@outlook.com>
2024-08-04 20:33:02 -0500

    reverting changes

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -2,38 +2,7 @@ import React from 'react';
 import '../styles/Timeline.css'
 
   const Timeline = ({ title, subtitle, description, startDate, endDate, _id }) => {
-    return (
-    <section class="design-section">
-        <div class="timeline">
-                    <div class="timeline-empty"></div>
-    
-                    <div class="timeline-middle">
-                       <div class="timeline-circle"></div>
-                    </div>
-                    <div class="timeline-component timeline-content">
-                        <h3>HTML</h3>
-                        <p>Some Text</p>
-                    </div>
-                    <div class="timeline-component timeline-content">
-                        <h3>CSS</h3>
-                        <p>Some Text.</p>
-                    </div>
-                    <div class="timeline-middle">
-                        <div class="timeline-circle"></div>
-                    </div>
-                    <div class="timeline-empty"></div>
-                    <div class="timeline-empty"></div>
-                   <div class="timeline-middle">
-                       <div class="timeline-circle"></div>
-                   </div>
-                   <div class=" timeline-component timeline-content">
-                    <h3>Javascript</h3>
-                    <p>Some Text.</p>
-               </div>
-           </div>
-    </section>
-
-
+    return (<div>timeline</div>
     );
   };
 

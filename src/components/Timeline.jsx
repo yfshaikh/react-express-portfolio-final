85ab1b3ff86150769ad024ea1d5e2cbbc35ef605
Yusuf <yshaikh113@outlook.com>
2024-08-04 21:14:48 -0500

    updated timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import '../styles/Timeline.css';
 
-const Timeline = ({ title, subtitle, description, startDate, endDate, _id }) => {
+const Timeline = ({ items }) => {
   return (
-    <div className="timeline-component" key={_id}>
-      <div className="timeline-circle"></div>
-      <div className="timeline-content">
-        <h3>{title}</h3>
-        <h4>{subtitle}</h4>
-        <p>{description}</p>
+    <section className="design-section">
+      <div className="timeline">
+        <div className="timeline-line"></div>
+        {items.map((item, index) => (
+          <div key={item._id} className="timeline-component">
+            <div className="timeline-circle" style={{ top: `${index * 100}px` }}></div>
+            <div className="timeline-content">
+              <h3>{item.title}</h3>
+              <h4>{item.subtitle}</h4>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+    </section>
   );
 };
 
 export default Timeline;
+
 

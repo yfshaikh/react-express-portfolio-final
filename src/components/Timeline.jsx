9462225e7730dab53c549912ef641af8f3a5066f
Yusuf <yshaikh113@outlook.com>
2024-08-04 21:11:30 -0500

    fixed timeline styling

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,7 +4,7 @@ import '../styles/Timeline.css';
 const Timeline = ({ title, subtitle, description, startDate, endDate, _id }) => {
   return (
     <div className="timeline-component" key={_id}>
-      <div className="timeline-line"></div>
+      <div className="timeline-circle"></div>
       <div className="timeline-content">
         <h3>{title}</h3>
         <h4>{subtitle}</h4>

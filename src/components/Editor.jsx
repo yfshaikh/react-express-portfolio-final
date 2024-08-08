5a08567d648af5c96e854bd1cda4ad7dce7d2470
Yusuf <yshaikh113@outlook.com>
2024-08-08 07:23:08 -0500

    updated text editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,15 +3,22 @@ import ReactQuill from 'react-quill'
 
 function Editor({value, onChange}) {
 
+
   const modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link', 'image'],
-      ['clean']
-    ],
-  }
+    'syntax': true,
+    'toolbar': [
+      [{ 'font': [] }, { 'size': [] }],
+      [ 'bold', 'italic', 'underline', 'strike' ],
+      [{ 'color': [] }, { 'background': [] }],
+      [{ 'script': 'super' }, { 'script': 'sub' }],
+      [{ 'header': '1' }, { 'header': '2' }, 'blockquote', 'code-block' ],
+      [{ 'list': 'ordered' }, { 'list': 'bullet'}, { 'indent': '-1' }, { 'indent': '+1' }],
+      [ 'direction', { 'align': [] }],
+      [ 'link', 'image', 'video', 'formula' ],
+      [ 'clean' ]
+]
+}
+
 
   return (
     <ReactQuill 
